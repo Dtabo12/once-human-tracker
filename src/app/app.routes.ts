47c93362f2a1cd64cc5f2ee444d35ab7f6fb8a5e
Tabito <dtabodevsoft@gmail.com>
2024-08-20 20:51:47 -0500

    :construction:Add
    - add redirect to mods view
    - fill all the gold armar mods

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'mods',
+    pathMatch: 'full'
+  },
+  {
     path: 'mods',
     loadComponent: () =>
       import('./pages/mods-tracker/mods-tracker.component').then((a) => a.ModsTrackerComponent),
