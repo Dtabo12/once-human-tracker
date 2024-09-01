a18f83a7cedf31c48d6bd64b1389937e020e79ef
Tabito <dtabodevsoft@gmail.com>
2024-09-01 12:40:59 -0500

    :sparkles:Add
    - add settings JSON to avoid the tailwind warning
    - change and improve the look and fill creating a new empty home page
      and using the routing correctly (I guess)
    - add general classes
    - create a navigator with the posible routes and keeping the others
    - add an environment variables for links and url images

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const ENV = {
+  images: {
+    onceHumanLogo: 'https://www.oncehuman.game/pc/fab/20240706164218/img/logo_1098a5d6.png',
+    homeBGMobile: 'https://images.hdqwalls.com/download/once-human-4k-cd-1080x1920.jpg',
+    homeBGDesktop: 'https://images.hdqwalls.com/download/once-human-4k-cd-1920x1200.jpg'
+  },
+  usefullRoutes: {
+    officialSite: 'https://www.oncehuman.game/',
+    interactiveMap: 'https://oncehuman.th.gl/',
+    doomerisSheet: ''
+  }
+}
